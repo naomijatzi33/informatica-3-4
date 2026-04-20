821b528dc03bd52d44cb44f8cb81a542056aa0f4
naomijatzi33 <naomijatzil@gmail.com>
2026-04-20 18:19:32 +0000

    recipe

---
+++ recipe.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Coconut Cranberry Dessert \n");
+    printf("1 1/2 cups graham cracker crumbs\n");
+    printf("1/2 cup butter or margarine, melted\n");
+}
+
