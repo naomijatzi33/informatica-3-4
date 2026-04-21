b6ca8758340d0dca7b008f98a72621483b55397d
naomijatzi33 <naomijatzil@gmail.com>
2026-04-21 21:54:10 +0000

    naming variables

---
+++ naming-variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+int number; //The exclation mark is gone
+int testvalue; //There is no space needed in between test
+int example1; //The number was changed to the other side
+float int_count; //Count was added
+printf("Variables created successfully.\n");
+}
