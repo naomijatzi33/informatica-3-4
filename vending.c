3fb0ef5c18fd5870026956eaf7b63d4fd9eb84a9
naomijatzi33 <naomijatzil@gmail.com>
2026-04-23 15:08:08 +0000

    Recipe

---
+++ vending.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+    //Creat variables for first snack (Energy Drink)
+    float price1 = 129.99;
+    int Quantity available = 20;
+    char Selection Code = "A";
+    //Creat variables for first snack (Gummies)
+    float price1 = 25;
+    int Quantity available = 15;
+    char Selection Code = "B";
+    //Creat variables for first snack (Marshmellows)
+    float price1 = 30;
+    int Quantity available = 10;
+    char Selection Code = "C";
+    
+    printf("Vending Machine Stock\n");
+}
