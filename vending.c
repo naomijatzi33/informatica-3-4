d97251014aee4e3d3961151b55f1d059d1075ada
naomijatzi33 <naomijatzil@gmail.com>
2026-04-23 15:16:54 +0000

    Coconut

--- vending.c
+++ vending.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 int main(void)
 {
-    //Creat variables for first snack (Energy Drink)
-    float price1 = 129.99;
+    //Creat variables for first snack (Monster)
+    float price1 = 70.99;
     int Quantity available = 20;
     char Selection Code = "A";
     //Creat variables for first snack (Gummies)
-    float price1 = 25;
+    float price1 = 25.99;
     int Quantity available = 15;
     char Selection Code = "B";
     //Creat variables for first snack (Marshmellows)
-    float price1 = 30;
+    float price1 = 30.99;
     int Quantity available = 10;
     char Selection Code = "C";
-    
+
     printf("Vending Machine Stock\n");
+    printf("---------------------------\n");
+    printf("Item name: Monster\n");
+    printf("Price: %.2f \n", price1);
+
 }
