9c9b1c39294e92dc0012329e8c29a4c9873936d4
naomijatzi33 <naomijatzil@gmail.com>
2026-04-24 14:48:02 +0000

    User

--- hello.c
+++ hello.c
@@ -2,5 +2,16 @@
 
 int main(void)
 {
-printf("Hello Jatzi \n");
+    char UserName[50];
+    printf ("User Name: ");
+    scanf("%s" , &UserName);
+    printf("Hello Jatzi \n");
+    printf("User Name is: %s \n", UserName);
+
+    char FavColor[50];
+    printf ("Favorite Color: ");
+    scanf("%s" , &FavColor);
+    printf("Favorite Color: \n");
+    printf("Favorite Color is: %s \n", FavColor);
 }
+
