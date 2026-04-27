3f67fb26ea643c7e6375628d2969d76617ad7729
naomijatzi33 <naomijatzil@gmail.com>
2026-04-27 14:59:58 +0000

    aaaa

---
+++ temperature.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int  Fahrenheit;
+    printf("Fahrenheit:");
+    scanf("%d" , &Fahrenheit);
+    int Celsius;
+    printf("Celsius:");
+    scanf("%d" , &Celsius);
+}
