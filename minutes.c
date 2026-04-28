39c278960c30cd7bffd69856e96cf734a5cf0158
naomijatzi33 <naomijatzil@gmail.com>
2026-04-28 21:59:08 +0000

    minutes

---
+++ minutes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int minutes;
+    printf("Movie Running Time:");
+    scanf("%d" , &minutes);
+    int time;
+    printf("Start time:");
+    scanf("%d" , &minutes);
+
+    int minutes=/60;
+    int minutes:%50;
+    printf("The movie will last:%d and %d min \n", minutes, min);
+
+
+
+}
