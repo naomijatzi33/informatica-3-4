84291acb2add62f9a5d95f76cdce2bd6d6a8928b
naomijatzi33 <naomijatzil@gmail.com>
2026-04-29 17:24:12 +0000

    Finished

---
+++ test/receipt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int item;
+    printf("Name of the item:");
+    scanf("%s" , &item);
+    int price;
+    printf("Price of the item:");
+    scanf("%d" , &price);
+    int quantity;
+    printf("Quantity of items:");
+    scanf("%d" , &quantity);
+
+
+    printf("\n");
+    printf("**********************************\n");
+    printf("Cool Store Name\n");
+    printf("**********************************\n");
+
+    printf("Item        Quantity        Price\n");
+    printf("Cereal        2        $50.00\n");
+    printf("-----------------------------------\n");
+    int total = price*quantity;
+    printf ("total: %d \n", total);
+    printf("-----------------------------------\n");
+    printf("Thank you for your purchase!\n");
+
+
+
+}
