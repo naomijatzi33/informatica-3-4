f88a8a669382d749e1906be1f7f5ab2baa1d36ab
naomijatzi33 <naomijatzil@gmail.com>
2026-05-12 21:58:06 +0000

    rhdfg

---
+++ Week4/temp3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+//prototype
+ void c_to_f(void);
+ void f_to_c(void);
+
+int main (void){
+
+    int  user_response;
+    printf("Temperature converter\n");
+    printf("select an option\n");
+    printf("1.Celsius to Fahrenheit\n");
+    printf("2.Fahrenheit to Celsius\n");
+    scanf("%d" , &user_response);
+
+    if (user_response == 1){
+        c_to_f();
+    }else if (user_response == 2){
+        f_to_c();
+    }else{
+        printf("invalid option\n");
+    }
+
+}
+//2.definition
+void c_to_f(void){
+    int c;
+    printf("Enter c:  ");
+    scanf("%d", &c);
+
+    int f = (c*1.8)+32;
+    printf("%dc° = %df° \n" , c,f);
+}
+
+void f_to_c(void){
+    int f;
+    printf("Enter fahrenheit: ");
+    scanf("%d" ,&f);
+
+    int c = (f-32)/1.8;
+    printf("%df° = %dc° \n" , f,c);
+}
